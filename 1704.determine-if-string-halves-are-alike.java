ec3d8d6ff49db3cf5a25725c7dba12a66c06ece5
Your Name <you@example.com>
2024-01-12 09:02:21 +0700

    1704-88%

--- 1704.determine-if-string-halves-are-alike.java
+++ 1704.determine-if-string-halves-are-alike.java
@@ -6,46 +6,26 @@
 
 // @lc code=start
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 
 class Solution {
     public boolean halvesAreAlike(String s) {
-        // Tạo HashSet để lưu trữ các ký tự
-        HashSet<Character> charSet = new HashSet<>();
-
-        // Thêm các ký tự vào HashSet
-        charSet.add('a');
-        charSet.add('e');
-        charSet.add('i');
-        charSet.add('o');
-        charSet.add('u');
-        charSet.add('A');
-        charSet.add('E');
-        charSet.add('I');
-        charSet.add('O');
-        charSet.add('U');
-
-        boolean ans = true;
         int count1 = 0;
         int count2 = 0;
-        int length = s.length();
-        for (int i = 0; i < length / 2; i++) {
-            if (charSet.contains(s.charAt(i))) {
+        int mid = s.length() / 2;
+        String firstHalf = s.substring(0, mid);
+        String secondHalf = s.substring(mid);
+        for (char ch : firstHalf.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(ch) != -1) {
                 count1++;
             }
         }
-        for (int i = length / 2; i < length; i++) {
-            if (charSet.contains(s.charAt(i))) {
+        for (char ch : secondHalf.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(ch) != -1) {
                 count2++;
             }
         }
-        if (count1 != count2) {
-            ans = false;
-        }
-        return ans;
+ 
+        return count1 == count2;
     }
 }
 // @lc code=end
